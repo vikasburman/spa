bcaa7f98d7b6292ec451318ab1010d9e4b53e7b0
Vikas Burman <vikasburman@yahoo.com>
2019-08-12 20:45:10 -0400

    Added support for 'inactive'

--- updates.js
+++ updates.js
@@ -1,5 +1,5 @@
 window.spaLatestVersion = {
-	versionId: 0.94,
-	url: "https://github.com/vikasburman/spa/releases/tag/v0.9.4",
-	title: "Version 0.9.4",
+	versionId: 1.10,
+	url: "https://github.com/vikasburman/spa/releases/tag/v1.1.0",
+	title: "Version 1.1.0",
 };
