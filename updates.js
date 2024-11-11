7137d0b8c0426b309c3b42a3a49cd9925f900e02
Vikas Burman <vikasburman@gmail.com>
2024-11-11 13:03:22 -0500

    v2.2.0

--- updates.js
+++ updates.js
@@ -1,5 +1,5 @@
 window.spaLatestVersion = {
-	versionId: 1.10,
-	url: "https://github.com/vikasburman/spa/releases/tag/v1.1.0",
-	title: "Version 1.1.0",
+	versionId: 2.20,
+	url: "https://github.com/vikasburman/spa/releases/tag/v2.2.0",
+	title: "Version 2.2.0",
 };
